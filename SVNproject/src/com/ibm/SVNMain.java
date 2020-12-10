82440d6dccbb56b97d71df2fdf9d390816a65a31
Marisa5373 <Marisa5373@107badb8-7558-4cf1-89ff-8d7e41c86cb2>
2020-12-10 14:29:32 +0000

    git-svn-id: https://svn.riouxsvn.com/myrep5373@25 107badb8-7558-4cf1-89ff-8d7e41c86cb2

--- SVNproject/src/com/ibm/SVNMain.java
+++ SVNproject/src/com/ibm/SVNMain.java
@@ -5,12 +5,7 @@ public class SVNMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("hello");
-		System.out.print("hello fix 1");
-		System.out.print("hello fix 1.1");
-
-		System.out.print("hello fix 1.2");
-
-		System.out.print("hello fix 2.2");
+		
 
 	}
 
